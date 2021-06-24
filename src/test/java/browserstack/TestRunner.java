5770083cdedd90176943988a620f527e4852156c
Sanket Mali <Sanket.M@Sanket-Malis-MacBook-Pro.local>
2021-06-24 18:55:22 +0530

    added yml file reading logic

--- src/test/java/browserstack/TestRunner.java
+++ src/test/java/browserstack/TestRunner.java
@@ -1,7 +1,12 @@
 package browserstack;
 
+import java.net.MalformedURLException;
+
+import org.openqa.selenium.WebDriver;
 import org.testng.annotations.*;
 
+import browserstack.driverconfig.Platform;
+import browserstack.driverconfig.WebDriverFactory;
 import browserstack.stepdefs.BaseTest;
 import browserstack.utils.AllureReportConfigurationSetup;
 import io.cucumber.java.Before;
@@ -12,8 +17,16 @@ import io.cucumber.testng.PickleWrapper;
 import io.cucumber.testng.TestNGCucumberRunner;
 
 @CucumberOptions(features = "src/test/resources/com/browserstack", glue = "browserstack.stepdefs")
-public class ParallelTestRunner extends BaseTest {
+public class TestRunner   {
+	
+	private WebDriverFactory webDriverFactory = new WebDriverFactory();
+	private Platform platform = new Platform();
+	private WebDriver webDriver;
+	
+	
+
 	private TestNGCucumberRunner testNGCucumberRunner;
+	private String testMethodName;
 	
 	@BeforeSuite
 	public void setupReport()
@@ -26,6 +39,22 @@ public class ParallelTestRunner extends BaseTest {
 		testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
 		
 	}
+	
+	
+	
+
+	@BeforeMethod
+	public WebDriver createDriver() throws MalformedURLException {
+		String[] specificCapabilities = new String[0];
+		if (this.webDriver == null) {
+			this.webDriver = this.webDriverFactory.createWebDriverForPlatform(this.platform, 
+this.testMethodName,
+					specificCapabilities);
+		}
+		return this.webDriver;
+	}
+
+	
 
 	@Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "scenarios")
 	public void feature(PickleWrapper pickleWrapper, FeatureWrapper featureWrapper) {
