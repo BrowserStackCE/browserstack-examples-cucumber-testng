55219110934482ad6e6dc985a0f1a31b4ce455ee
sanket m <Sanket.ma@browserstack.com>
2021-06-29 19:55:59 +0530

    - gradle profile updated ( work in progress ) - updated yml profile

--- src/test/java/com/browserstack/examples/core/WebDriverFactory.java
+++ src/test/java/com/browserstack/examples/core/WebDriverFactory.java
@@ -46,7 +46,7 @@ public class WebDriverFactory {
     private static final String BROWSERSTACK_USERNAME = "BROWSERSTACK_USERNAME";
     private static final String BROWSERSTACK_ACCESS_KEY = "BROWSERSTACK_ACCESS_KEY";
     private static final String BUILD_ID = "BUILD_ID";
-    private static final String DEFAULT_BUILD_NAME = "browserstack-examples-junit5";
+    private static final String DEFAULT_BUILD_NAME = "browserstackexamplestestng";
 
     private static final String WEBDRIVER_CHROME_DRIVER = "webdriver.chrome.driver";
     private static final String WEBDRIVER_GECKO_DRIVER = "webdriver.gecko.driver";
