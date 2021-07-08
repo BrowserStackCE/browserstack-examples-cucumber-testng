3073afea11c82021797433ffcc0b4ea5ec958490
Sanket Mali <Sanket.M@Sanket-Malis-MacBook-Pro.local>
2021-07-09 03:47:32 +0530

    fixed gradle profiles and updated allure

--- src/test/java/com/browserstack/examples/tests/RunWebDriverCucumberTests.java
+++ src/test/java/com/browserstack/examples/tests/RunWebDriverCucumberTests.java
@@ -27,7 +27,7 @@ import io.cucumber.testng.TestNGCucumberRunner;
  * @author Anirudha Khanna
  */
 @CucumberOptions(features = "classpath:features", glue = "com.browserstack.examples.stepdefs", plugin = {
-		"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", "pretty", "json:target/allure-results/report.json" })
+		"io.qameta.allure.cucumber6jvm.AllureCucumber6Jvm", "pretty", "json:allure-results/report.json" })
 public class RunWebDriverCucumberTests {
 
 	private TestNGCucumberRunner testNGCucumberRunner;
@@ -74,7 +74,7 @@ public class RunWebDriverCucumberTests {
 
 	@AfterSuite
 	public void ReportGeneration() {
-		Utility.moveFolder();
+		//Utility.moveFolder();
 	}
 
 }
